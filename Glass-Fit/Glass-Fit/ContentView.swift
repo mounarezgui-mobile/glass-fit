513ba587eaf50380484da759b436fba6e2dfed5f
Mouna Rezgui <122633673+mounarezgui-mobile@users.noreply.github.com>
2023-06-02 13:21:18 +0100

    Initial project setup

---
+++ Glass-Fit/Glass-Fit/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  Copyright (c) 2023, Mouna Rezgui. All rights reserved.
+//  This source code is licensed under the GNU General Public License v3.0 license
+//  found in the LICENSE file in the root directory of this source tree.
+//
+//  ContentView.swift
+//  Glass-Fit
+//
+//  Created by Mouna Rezgui on 2/6/2023.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ Glass-Fit/Glass-FitTests/Glass_FitTests.swift
@@ -0,0 +1,32 @@
+//
+//  Copyright (c) 2023, Mouna Rezgui. All rights reserved.
+//  This source code is licensed under the GNU General Public License v3.0 license
+//  found in the LICENSE file in the root directory of this source tree.
+//
+//  Glass_FitTests.swift
+//  Glass-FitTests
+//
+//  Created by Mouna Rezgui on 2/6/2023.
+//
+
+import XCTest
+@testable import Glass_Fit
+
+final class Glass_FitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+}
