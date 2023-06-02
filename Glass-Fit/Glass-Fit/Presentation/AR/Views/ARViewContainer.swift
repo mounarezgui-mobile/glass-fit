41b983242d415b174ef2c7b85cf73fbde7727050
Mouna Rezgui <122633673+mounarezgui-mobile@users.noreply.github.com>
2023-06-02 14:30:49 +0100

    chore: remove unused auto-generated reality kit code

--- Glass-Fit/Glass-Fit/Presentation/AR/Views/ARViewContainer.swift
+++ Glass-Fit/Glass-Fit/Presentation/AR/Views/ARViewContainer.swift
@@ -18,12 +18,6 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
         return arView
         
     }
